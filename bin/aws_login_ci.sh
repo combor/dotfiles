efce7c931d61dbda9377bea23aaeb806db84eb37
Piotr Komborski <piotr.komborski@gmail.com>
2016-11-25 23:27:06 +0000

    AWS loging scripts

---
+++ bin/aws_login_ci.sh
@@ -0,0 +1,2 @@
+export AWS_ACCESS_KEY_ID=$(pass amazon/ci/AWS_ACCESS_KEY_ID)
+export AWS_SECRET_ACCESS_KEY=$(pass amazon/ci/AWS_SECRET_ACCESS_KEY)
---
+++ bin/aws_login_staging.sh
@@ -0,0 +1,2 @@
+export AWS_ACCESS_KEY_ID=$(pass amazon/staging/AWS_ACCESS_KEY_ID)
+export AWS_SECRET_ACCESS_KEY=$(pass amazon/staging/AWS_SECRET_ACCESS_KEY)
