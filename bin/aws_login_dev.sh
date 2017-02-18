a67642dd1c428fdd5add202c4af76e7de2fe6017
Piotr Komborski <piotr.komborski@gmail.com>
2017-02-18 21:44:04 +0000

    Update creds generate scripts

--- bin/aws_login_dev.sh
+++ bin/aws_login_dev.sh
@@ -2,3 +2,6 @@ export AWS_ACCESS_KEY_ID=$(pass amazon/dev/AWS_ACCESS_KEY_ID)
 export AWS_SECRET_ACCESS_KEY=$(pass amazon/dev/AWS_SECRET_ACCESS_KEY)
 export AWS_DEFAULT_REGION=eu-west-1
 export AWS_REGION=eu-west-1
+export AWS_ACCOUNT=dev
+export PAAS_CF_DIR="/go/src/github.com/alphagov/paas-cf"
+"${PAAS_CF_DIR}/scripts/create_sts_token.sh" && source "${HOME}/.aws_sts_tokens/${AWS_ACCOUNT}.sh"
